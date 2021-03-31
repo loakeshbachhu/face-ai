71ecfaccc2b7b91646ab8cc75553b9d0e0c3509f
Loakesh Bachhu <loakesh.bachhu@courts.mo.gov>
2021-03-31 14:54:12 -0500

    Error fixes

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -7,7 +7,7 @@ const ImageLinkForm = () => {
                 <p className='f4 center white'>
                     {'This face-ai will detect the faces. Upload & Try It!'}
                 </p>
-                <div class='center'>
+                <div className='center'>
                     <div  className='form pa4 br3 shadow-5 center'>
                         <input className='f4 pa2 w-75 center' type='text' placeholder='Enter or paste URL...'/>
                         <button className='w-25 grow f4 link ph3 br3 pv2 dib navy bg-light-red'>Analyze</button>
